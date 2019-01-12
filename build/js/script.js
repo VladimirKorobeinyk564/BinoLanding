fabed2360e4af8165814265628939b8782310918
Vladimir <vovakorobeinyk2001@gmail.com>
2019-01-12 17:24:23 +0300

    first commit

---
+++ build/js/script.js
@@ -0,0 +1,20 @@
+/*$(document).ready(function() {
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            loop:true,
+            items:1,
+            navText:true,
+            paginationSpeed:400
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:true
+        }
+    }
+  });
+});*/
